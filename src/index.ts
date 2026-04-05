2a32e5be8f86b79bd500efe72836b33661b67a76
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2026-04-05 06:45:53 +0600

    chore: bump package version to 1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
-import { setConfig } from "./config.js";
-import { upload, deleteFile } from "./s3.js";
-import { RRVaultConfig, UploadOptions, UploadResult, DeleteResult } from "./types.js";
-
-export class RRVault {
-
-  static config(config: RRVaultConfig) {
-    setConfig(config);
-  }
-
-  static async upload(
-    file: any,
-    fileName: string,
-    options: UploadOptions = {}
-  ): Promise<UploadResult> {
-    return await upload(file, fileName, options);
-  }
-
-  static async delete(key: string): Promise<DeleteResult> {
-    return await deleteFile(key);
-  }
-}
-
-export {
-  setConfig as config,
-  upload,
-  deleteFile
-};
-
-export type {
-  RRVaultConfig,
-  UploadOptions,
-  UploadResult,
-  DeleteResult
+import { setConfig } from "./config.js";
+import { upload, deleteFile } from "./s3.js";
+import { RRVaultConfig, UploadOptions, UploadResult, DeleteResult } from "./types.js";
+
+
+export class RRVault {
+
+  static config(config: RRVaultConfig) {
+    setConfig(config);
+  }
+
+  static async upload(
+    file: any,
+    fileName: string,
+    options: UploadOptions = {}
+  ): Promise<UploadResult> {
+    return await upload(file, fileName, options);
+  }
+
+  static async delete(key: string): Promise<DeleteResult> {
+    return await deleteFile(key);
+  }
+}
+
+export {
+  setConfig as config,
+  upload,
+  deleteFile
+};
+
+export type {
+  RRVaultConfig,
+  UploadOptions,
+  UploadResult,
+  DeleteResult
 };
